9bc276183d22d01296b1c32b41a6b8668eecefc1
blktech <github@blktech.com>
2019-02-10 18:15:17 +0000

    Auto Commit dom feb 10 18:15:17 UTC 2019

--- BLKTech/XML/Document/HTML.php
+++ BLKTech/XML/Document/HTML.php
@@ -1,18 +1,32 @@
 <?php
-
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
  */
 
 namespace BLKTech\XML\Document;
+use \BLKTech\XML\Document;
 
 /**
- * Description of HTML
  *
- * @author instalacion
+ * @author TheKito < blankitoracing@gmail.com >
  */
-class HTML {
-    //put your code here
+
+class HTML extends Document
+{
+    public function __construct() 
+    {        
+        parent();
+        parent::setDeclaration('!DOCTYPE html');               
+    }
+
 }
