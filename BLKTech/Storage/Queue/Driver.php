828b7a7a0a5d2d64162498f61e5fbc525251f492
blktech <github@blktech.com>
2019-02-24 23:16:05 +0000

    Auto Commit dom feb 24 23:16:05 UTC 2019

---
+++ BLKTech/Storage/Queue/Driver.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace BLKTech\Storage\Queue;
+
+/**
+ * Description of Driver
+ *
+ * @author instalacion
+ */
+
+abstract class Driver{
+
+
+
+}
